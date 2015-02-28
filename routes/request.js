39f7dc7cbcb79f570d94176762cee5c703fedd3a
timothysu <timmys.soup@gmail.com>
2015-02-28 16:30:01 -0600

    Add Post Function

--- routes/request.js
+++ routes/request.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
   var jsonBuilder = [];
   jsonBuilder.push({cityFrom: req.body.cityFrom, cityTo: req.body.cityTo, startDate: req.body.startDate, endDate: req.body.endDate});
   res.contentType('application/json');
