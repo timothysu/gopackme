8908b1d0642c32d162bf7c9ff5bf8a42ad47f955
timothysu <timmys.soup@gmail.com>
2015-02-28 20:05:20 -0600

    Oops

--- public/js/app.js
+++ public/js/app.js
@@ -46,28 +46,6 @@
 	}
 
 
-	Sortable.create(byId('foo'), {
-		group: "words",
-		animation: 150,
-		store: {
-			get: function (sortable) {
-				var order = localStorage.getItem(sortable.options.group);
-				return order ? order.split('|') : [];
-			},
-			set: function (sortable) {
-				var order = sortable.toArray();
-				localStorage.setItem(sortable.options.group, order.join('|'));
-			}
-		},
-		onAdd: function (evt){ console.log('onAdd.foo:', [evt.item, evt.from]); },
-		onUpdate: function (evt){ console.log('onUpdate.foo:', [evt.item, evt.from]); },
-		onRemove: function (evt){ console.log('onRemove.foo:', [evt.item, evt.from]); },
-		onStart:function(evt){ console.log('onStart.foo:', [evt.item, evt.from]);},
-		onSort:function(evt){ console.log('onStart.foo:', [evt.item, evt.from]);},
-		onEnd: function(evt){ console.log('onEnd.foo:', [evt.item, evt.from]);}
-	});
-
-
 	// Angular example
 	angular.module('todoApp', ['ng-sortable'])
 		.controller('TodoController', ['$scope', function ($scope) {
