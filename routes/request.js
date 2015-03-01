fcc9c498ce1ab57b32ae5ec4d0cd41960270343d
timothysu <timmys.soup@gmail.com>
2015-03-01 01:47:25 -0600

    wtf

--- routes/request.js
+++ routes/request.js
@@ -8,6 +8,8 @@ router.post('/', function(req, res) {
 
   var exec = require('child_process').exec;
   exec('java -cp DFW.jar Main "' + jsonObj['tags'][0] + '"', function (error, stdout, stderr) {
+    if(error) console.log(error);
+    if(stderr) console.log(stderr);
     res.contentType('application/json');
     res.send(JSON.stringify(stdout));
   });
