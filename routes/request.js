3ad0ee9d31b3db9d2ff8c1b2134d87b0e490ff08
timothysu <timmys.soup@gmail.com>
2015-03-01 02:05:05 -0600

    Process JSON

--- routes/request.js
+++ routes/request.js
@@ -5,11 +5,13 @@ var router = express.Router();
 router.post('/', function(req, res) {
   //console.log(req.body);
   var jsonObj = req.body;
+  jsonObj['tags'] = jsonObj['tags[]'];
+  delete jsonObj['tags[]'];
   //console.log(jsonObj['tags[]'][0]);
   //jsonBuilder.push({cityFrom: req.body.cityFrom, cityTo: req.body.cityTo, startDate: req.body.startDate, endDate: req.body.endDate});
 
   var exec = require('child_process').exec;
-  var command = 'java -cp DFW.jar Main "' + jsonObj['tags[]'][0] + '"';
+  var command = 'java -cp DFW.jar Main "' + JSON.stringify(jsonObj) + '"';
   console.log(command);
   exec(command, function (error, stdout, stderr) {
     if(error) console.log(error);
